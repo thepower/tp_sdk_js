56fd807500a71309af58ff9cf3c250cd10d95332
al <al@thepower.io>
2021-02-21 02:04:33 +0300

    Update

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 //const addressLib = require('./address-lib');
 const {packAndSignTX, composeSCMethodCallTX} = require('./transactions-lib');
 const {sendTxAndWaitForResponse, getFeeSettings, setChain} = require('./network-lib');
-//const scInterface = require('./sc_interface');
-const scLoader = require('./sc_loader');
+const scInterface = require('./sc_interface');
+//const scLoader = require('./sc_loader');
 const {decryptWif} = require('./crypto-lib');
 const accountLib = require('./account-lib');
+const msgPack = require('./tp_msgpack/msgpack-lite');
 
 const loadFeeSettings = async (chain) => {
     await setChain(chain);
     return await getFeeSettings();
 }
+
+const loadScLocal = (code, state = {}, balance = {}) => new scInterface(code, msgPack.encode(state), balance);
 
 module.exports = {
     ...accountLib,
@@ -17,5 +20,6 @@ module.exports = {
     packAndSignTX,
     loadFeeSettings,
     composeSCMethodCallTX,
-    sendTxAndWaitForResponse
+    sendTxAndWaitForResponse,
+    loadScLocal
 };
